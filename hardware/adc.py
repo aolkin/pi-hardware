952cc301fbd0b0b60cc8660904456a81c2ec2f91
Aaron Olkin <aaronolkin@gmail.com>
2015-06-17 16:36:42 -0400

    ADC bugfixes

--- hardware/adc.py
+++ hardware/adc.py
@@ -13,7 +13,7 @@ from threading import Thread
 
 IC = 0 # ADS1015 (12-bit ADC)
 GAIN = 0x1000 # (+/- 4.096V for Rasberry Pi 3V3 supply)
-SPS = 2400
+SINGLE_SPS = SPS = 2400
 WAITMS = 100
 
 MILLIVOLTS = 1000
@@ -23,13 +23,14 @@ class ADC4(I2CComponent, EventedInput):
         super().__init__(*args, **kwargs)
         self.thread = Thread(target=self.runloop)
         self.thread.daemon = True
-        self.thread.start()
         self.values = [None, None, None, None]
         self.__started = False
+        self.thread.start()
 
     def init(self, autostart=False):
         super().init()
-        self.adc = ADS1x15(ic=IC, address=self._addr)
+        self.adc = ADS1x15(ic=IC, address=self._address)
+        self._set_init()
         if autostart:
             self.start()
 
@@ -49,13 +50,15 @@ class ADC4(I2CComponent, EventedInput):
         return self.values[pin]
 
     def read(self, pin):
-        return self.process_reading(self.adc.readADCSingleEnded(pin, GAIN, SINGLE_SPS))
+        self._checkInit()
+        return self.process_reading(
+            self.adc.readADCSingleEnded(pin, GAIN, SINGLE_SPS))
     def process_reading(self, reading):
         return reading / MILLIVOLTS
 
     def runloop(self):
         while True:
-            if __started:
+            if self.__started:
                 for i in range(4):
                     v = self.read(i)
                     if self.values[i] != v:
@@ -63,15 +66,16 @@ class ADC4(I2CComponent, EventedInput):
                         self._handle_pin(i)
             waitms(50)
 
-def ScaledADC4(ADC4):
-    def __init__(self, low=0, high=GAIN, precision=0, scale=100,
-                 *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class ScaledADC4(ADC4):
+    # 34 - 3272
+    def __init__(self, addr, low=0, high=GAIN, precision=0, scale=100,
+                 **kwargs):
+        super().__init__(addr, **kwargs)
         self._low = low
-        self._high = high - low
+        self._high = high
         self._precision = precision
         self._scale = scale
 
     def process_reading(self, reading):
-        r = (reading - self._low) / self._high
+        r = (reading - self._low) / (self._high - self._low)
         return round(r * self._scale, self._precision)
